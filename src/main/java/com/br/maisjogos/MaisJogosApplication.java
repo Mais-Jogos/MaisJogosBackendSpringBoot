54d5b2e6a8e53f7020eb385bbdbae5e6d27683ff
Vicenzzo <vicenzzo.toth@gmail.com>
2023-09-10 00:09:27 -0300

    Primeiro commit

---
+++ src/main/java/com/br/maisjogos/MaisJogosApplication.java
@@ -0,0 +1,13 @@
+package com.br.maisjogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/maisjogos/MaisJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.maisjogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
