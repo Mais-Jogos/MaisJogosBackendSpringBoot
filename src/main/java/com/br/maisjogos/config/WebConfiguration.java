30da862db158a6bf70f50edf12294ea640535ea6
Vicenzzo <vicenzzo.toth@gmail.com>
2023-09-12 08:28:24 -0300

    :globe_with_meridians: :hammer: Adicionado a configuração web, do registry para os metodos da web

--- src/main/java/com/br/maisjogos/config/WebConfiguration.java
+++ src/main/java/com/br/maisjogos/config/WebConfiguration.java
@@ -1,5 +1,13 @@
 package com.br.maisjogos.config;
 
-public class WebConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer{
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("*");
+	}
+	
 }
