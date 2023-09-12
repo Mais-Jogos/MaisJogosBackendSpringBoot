158f1293ab269687a67ab331dd45b87fbc7b773e
Vicenzzo <vicenzzo.toth@gmail.com>
2023-09-12 08:37:30 -0300

    :bookmark: :heavy_plus_sign: metodo health criado para a saude da nossa aplicação

--- src/main/java/com/br/maisjogos/controller/HealthController.java
+++ src/main/java/com/br/maisjogos/controller/HealthController.java
@@ -1,5 +1,19 @@
 package com.br.maisjogos.controller;
 
-public class HealthController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/mais-jogos/health-check")
+public class HealthController {
+	
+	@GetMapping
+	public long health() {
+		return System.currentTimeMillis();
+	}
+	@GetMapping("/jogos")
+	public long health2() {
+		return System.currentTimeMillis();
+	}
 }
