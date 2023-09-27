66302679a13f5b698b6bcb55d3f8362f8c23773a
BiaJL <bianca.lima13@fatec.sp.gov.br>
2023-09-27 16:29:00 -0300

    Entity da Review

--- src/main/java/com/br/maisjogos/controller/ReviewController.java
+++ src/main/java/com/br/maisjogos/controller/ReviewController.java
@@ -1,9 +1,7 @@
 package com.br.maisjogos.controller;
 
-import com.br.maisjogos.entity.Review;
 
 public class ReviewController {
-    Review review = new Review();
-    String nome = this.review.getDataReview();
+    
     
 }
--- src/main/java/com/br/maisjogos/entity/Review.java
+++ src/main/java/com/br/maisjogos/entity/Review.java
@@ -4,90 +4,64 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
-@SuppressWarnings("serial")
-@Getter
-@Setter
-@EqualsAndHashCode(of = "id")
-@Entity(name = "review")
+
+@Entity
 public class Review {
-    @Id
-	@GeneratedValue(strategy = GenerationType.UUID)
-    @NotBlank(message = "O campo id é obrigatótio")
+	@Id
     private String id;
 
-    private Jogo jogoReview;
-
-    private Cliente clienteReview;
-
-    @NotNull(message = "A nota da Review é obrigatória")
     private double notaReview;
 
     private String dataReview;
 
-    @NotBlank(message="A descrição da Review é obrigatória")
     private String descricaoReview;
+    
+    
+    //Construtor geral
+	public Review(String id, double notaReview, String dataReview, String descricaoReview){
+		this.id = id;
+		this.notaReview = notaReview;
+		this.dataReview = dataReview;
+		this.descricaoReview = descricaoReview;
+	} 
+	
+	//Construtor vazio
+	public Review() {
+	}
 
-    public Review(Jogo jogoReview, double notaReview, String dataReview, String descricaoReview) {
-        this.jogoReview = jogoReview;
-        this.notaReview = notaReview;
-        this.dataReview = dataReview;
-        this.descricaoReview = descricaoReview;
-    }
 
-    public Review() {
-    }
+	//Getters e Setters
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public double getNotaReview() {
+		return notaReview;
+	}
 
-    public void setJogoReview(Jogo jogoReview) {
-        this.jogoReview = jogoReview;
-    }
+	public void setNotaReview(double notaReview) {
+		this.notaReview = notaReview;
+	}
 
-    public Jogo getJogoReview() {
-        return jogoReview;
-    }
+	public String getDataReview() {
+		return dataReview;
+	}
 
-    public void setClienteReview(Cliente clienteReview) {
-        this.clienteReview = clienteReview;
-    }
+	public void setDataReview(String dataReview) {
+		this.dataReview = dataReview;
+	}
 
-    public Cliente getClienteReview() {
-        return clienteReview;
-    }
+	public String getDescricaoReview() {
+		return descricaoReview;
+	}
 
-    public void setNotaReview(Double notaReview) {
-        this.notaReview = notaReview;
-    }
-
-    public Double getNotaReview() {
-        return notaReview;
-    }
-
-    public void setDataReview(String dataReview) {
-        this.dataReview = dataReview;
-    }
-
-    public String getDataReview() {
-        return dataReview;
-    }
-
-    public void setDescricaoReview(String descricaoReview) {
-        this.descricaoReview = descricaoReview;
-    }
-    public String getDescricaoReview() {
-        return descricaoReview;
-    }
+	public void setDescricaoReview(String descricaoReview) {
+		this.descricaoReview = descricaoReview;
+	}
 
 }
